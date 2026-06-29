199c0481b875cd7b3de8cbbafd946e95660f52f7
Akhilesh G <akhilesh.gollapudi@outlook.com>
2026-06-29 17:29:08 -0600

    Changed build and packaging, and added Autoupdate

--- main.js
+++ main.js
@@ -1,39 +1,52 @@
-const { app, BrowserWindow, ipcMain, shell } = require('electron');
+const { app, BrowserWindow, ipcMain, shell, session } = require('electron');
 const { autoUpdater } = require('electron-updater');
+const log = require('electron-log');
 const path = require('path');
 
 let mainWindow;
+
+// Wire auto-updater to electron-log so we can see what's happening
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
 
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
-    icon: path.join(__dirname, 'assets/icon.png'), // Default icon (PNG)
+    icon: path.join(__dirname, 'assets/icon.png'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: false,
       contextIsolation: true,
       enableRemoteModule: false,
       sandbox: true,
-      contentSecurityPolicy: `
-        default-src 'self';
-        script-src 'self' 'unsafe-inline';
-        connect-src https://api.earthmc.net;
-        img-src 'self' https://mc-heads.net;
-        style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-        font-src https://fonts.gstatic.com;
-      `
     }
+  });
+
+  // Set CSP via response headers (correct way)
+  session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
+    callback({
+      responseHeaders: {
+        ...details.responseHeaders,
+        'Content-Security-Policy': [
+          "default-src 'self'; " +
+          "script-src 'self' 'unsafe-inline'; " +
+          "connect-src https://api.earthmc.net https://api.github.com; " +
+          "img-src 'self' https://mc-heads.net data:; " +
+          "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
+          "font-src https://fonts.gstatic.com;"
+        ]
+      }
+    });
   });
 
   mainWindow.loadFile('renderer/index.html');
 
-  // Check for updates after window loads
   mainWindow.webContents.on('did-finish-load', () => {
+    log.info('Window loaded, checking for updates...');
     autoUpdater.checkForUpdatesAndNotify();
   });
 
-  // Open DevTools in development
   if (process.env.NODE_ENV === 'development') {
     mainWindow.webContents.openDevTools();
   }
@@ -42,36 +55,28 @@ function createWindow() {
 app.on('ready', createWindow);
 
 // Auto updater events
+autoUpdater.on('checking-for-update', () => log.info('Checking for update...'));
 autoUpdater.on('update-available', (info) => {
+  log.info('Update available:', info.version);
   mainWindow.webContents.send('update-available', info.version);
 });
-
+autoUpdater.on('update-not-available', () => log.info('Already up to date.'));
 autoUpdater.on('update-downloaded', (info) => {
+  log.info('Update downloaded:', info.version);
   mainWindow.webContents.send('update-ready', info.version);
 });
-
 autoUpdater.on('error', (err) => {
-  console.error('Auto updater error:', err);
+  log.error('Auto updater error:', err);
 });
 
-// IPC handlers
-ipcMain.on('install-update', () => {
-  autoUpdater.quitAndInstall();
-});
+// IPC
+ipcMain.on('install-update', () => autoUpdater.quitAndInstall());
+ipcMain.on('open-external', (_, url) => shell.openExternal(url));
 
-ipcMain.on('open-external', (_, url) => {
-  shell.openExternal(url);
-});
-
-// Window lifecycle
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+  if (process.platform !== 'darwin') app.quit();
 });
 
 app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow();
-  }
+  if (mainWindow === null) createWindow();
 });
